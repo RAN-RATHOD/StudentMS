016257924c5f37a1a33235e29076337d2e0bb2ff
RAN-RATHOD <64397601+RAN-RATHOD@users.noreply.github.com>
2024-07-12 16:02:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-// test-cookie-access.js
 console.log("External script executed");
-console.log("Document cookies:", document.cookie);
+
+var cookies = document.cookie;
+
+var message = "Issue 1 - Script Execution Success.";
+if (cookies) {
+    message += " Issue 2 - Cookies were Accessed by external script (Cookies - " + cookies + ")";
+} else {
+    message += " Issue 2 - No cookies were accessed by external script.";
+}
+
+// Display the popup
+alert(message);
